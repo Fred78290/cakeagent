be8c6151f15b07466b8e6a68f45d7145e817c9d4
Fred78290 <frederic.boltz@gmail.com>
2025-05-01 12:45:06 +0200

    HELPER: Update scripts

--- scripts/cakeagent-darwin.sh
+++ scripts/cakeagent-darwin.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-rm -rf ./.build *.o *.d *.swiftdeps
+#rm -rf ./.build *.o *.d *.swiftdeps
 
 swift build && exec ./.build/debug/cakeagent run \
+	--log-level=debug \
 	--listen=tcp://127.0.0.1:5000 \
 	--ca-cert="${HOME}/.cake/agent/ca.pem" \
 	--tls-cert="${HOME}/.cake/agent/server.pem" \
