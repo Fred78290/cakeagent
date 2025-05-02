c706ea99b5bed007f81696f76e9a22873ad78821
Fred78290 <frederic.boltz@gmail.com>
2025-05-02 14:48:29 +0200

    Bump dependency version

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.1"),
-		.package(url: "https://github.com/grpc/grpc-swift.git", from: "1.24.2"),
+		.package(url: "https://github.com/grpc/grpc-swift.git", from: "1.25.0"),
 		.package(url: "https://github.com/apple/swift-nio.git", from: "2.82.0"),
 		.package(url: "https://github.com/apple/swift-protobuf.git", from: "1.29.0"),
 		.package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
