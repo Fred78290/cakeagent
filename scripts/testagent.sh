8af4aea8294294afbf1f4d7e22e01a9132b79192
Fred78290 <frederic.boltz@gmail.com>
2025-05-17 17:52:38 +0200

    FEAT: Change listen port
    FEAT: Change listen port

--- scripts/testagent.sh
+++ scripts/testagent.sh
@@ -5,7 +5,7 @@ ROOT=${HERE}/..
 #	--connect=tcp://127.0.0.1:5000 \
 
 exec ${ROOT}/.build/debug/testagent \
-	--connect=tcp://127.0.0.1:5000 \
+	--connect=tcp://127.0.0.1:5010 \
 	--ca-cert="${HOME}/.cake/agent/ca.pem" \
 	--tls-cert="${HOME}/.cake/agent/client.pem" \
 	--tls-key="${HOME}/.cake/agent/client.key" $@
