c0b96bdc34adce40828d8439531e8026ba86b3cb
Fred78290 <frederic.boltz@gmail.com>
2025-11-21 18:18:36 +0100

    Updates build script for versioning and artifacts.
    Refactors the build script to improve version handling,
    ensuring accurate version information is embedded in the binaries.
    It also modifies the artifact creation process,
    creating platform-specific builds for Linux and Darwin using Go,
    and copies the created package to the artifacts directory.

--- .ci/build.sh
+++ .ci/build.sh
@@ -2,13 +2,12 @@
 set -ex
 
 CURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-GITHUB_REF=${GITHUB_REF:=$(git rev-parse HEAD)}
-VERSION="${GITHUB_REF##*/}"
+GITHUB_REF=${GITHUB_REF:=$(git rev-parse --short=8 HEAD)}
+VERSION="${GITHUB_REF##*/v}"
+BUILD_DATE=`date +%Y-%m-%dT%H:%M:%SZ`
 
 if [ -z "${VERSION}" ]; then
-	VERSION="SNAPSHOT"
-else
-	VERSION="${VERSION##*/v}"
+	VERSION="$(git rev-parse --short=8 HEAD)"
 fi
 
 export VERSION
@@ -17,20 +16,21 @@ TMPFILE=$(mktemp)
 
 envsubst < darwin/Sources/CakeAgent/CI/CI.swift > $TMPFILE
 cp $TMPFILE darwin/Sources/CakeAgent/CI/CI.swift
-cp $TMPFILE darwin/Sources/CakeAgent/CI/CI.swift
 
 swift build -c release --arch x86_64 --product cakeagent
 swift build -c release --arch arm64 --product cakeagent
 
 mkdir -p _artifacts
 pushd linux
-GOARCH=amd64 GOOS=linux go build -o ../_artifacts/cakeagent-linux-amd64
-GOARCH=arm64 GOOS=linux go build -o ../_artifacts/cakeagent-linux-arm64
-GOARCH=amd64 GOOS=darwin go build -o ../_artifacts/cakeagent-darwin-amd64
-GOARCH=arm64 GOOS=darwin go build -o ../_artifacts/cakeagent-darwin-arm64
+LDFLAGS="-s -w -X github.com/Fred78290/cakeagent/version.VERSION=${VERSION} -X github.com/Fred78290/cakeagent/version.BUILD_DATE={BUILD_DATE}"
+
+GOARCH=amd64 GOOS=linux go build  -ldflags="${LDFLAGS}" -o ../_artifacts/cakeagent-linux-amd64
+GOARCH=arm64 GOOS=linux go build -ldflags="${LDFLAGS}" -o ../_artifacts/cakeagent-linux-arm64
+GOARCH=amd64 GOOS=darwin go build -ldflags="${LDFLAGS}" -o ../_artifacts/cakeagent-darwin-amd64
+GOARCH=arm64 GOOS=darwin go build -ldflags="${LDFLAGS}" -o ../_artifacts/cakeagent-darwin-arm64
 popd
 
-if [ ${VERSION} != "SNAPSHOT" ]; then
+if [ ${VERSION} != "$(git rev-parse --short=8 HEAD)" ]; then
 	${CURDIR}/setup-keychain.sh
 	${CURDIR}/create-pkg.sh
 	security delete-keychain build.keychain
