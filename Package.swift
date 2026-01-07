3f9d935cecc65afd532cf1c34aca3737462c4cb5
Fred78290 <frederic.boltz@gmail.com>
2026-01-07 15:49:57 +0100

    Updates dependencies
    Bumps versions of several Swift packages to pick up the latest features and bug fixes.
    
    - swift-argument-parser: 1.5.0 -> 1.7.0
    - grpc-swift: 1.27.1 -> latest revision
    - swift-nio: 2.82.0 -> 2.92.1
    - swift-protobuf: 1.31.0 -> 1.33.3
    - swift-log: 1.6.2 -> 1.8.0
    - swift-nio-portforwarding: 0.2.2 -> 0.2.9

--- Package.swift
+++ Package.swift
@@ -15,21 +15,23 @@ let package = Package(
 		.library(name: "CakeAgentLib", targets: ["CakeAgentLib"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.1"),
-		.package(url: "https://github.com/grpc/grpc-swift.git", from: "1.27.1"),
-		.package(url: "https://github.com/apple/swift-nio.git", from: "2.82.0"),
-		.package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.0"),
-		.package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.7.0"),
+		.package(url: "https://github.com/grpc/grpc-swift.git", revision: "5770e231585cb68388a6ae37ce36c4d8dbd46d90"),
+		.package(url: "https://github.com/apple/swift-nio.git", from: "2.92.1"),
+		.package(url: "https://github.com/apple/swift-protobuf.git", from: "1.33.3"),
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.8.0"),
 		.package(url: "https://github.com/groue/Semaphore", from: "0.0.8"),
-		.package(url: "https://github.com/Fred78290/swift-nio-portforwarding.git", .upToNextMajor(from: "0.2.2")),
+		.package(url: "https://github.com/Fred78290/swift-nio-portforwarding.git", .upToNextMajor(from: "0.2.9")),
 		.package(url: "https://github.com/cfilipov/TextTable", branch: "master"),
 	],
 	targets: [
 		.target(name: "CakeAgentLib",
 			dependencies: [
+				.product(name: "TextTable", package: "TextTable"),
 				.product(name: "GRPC", package: "grpc-swift"),
 				.product(name: "Semaphore", package: "Semaphore"),
-				.product(name: "NIOPortForwarding", package: "swift-nio-portforwarding")
+				.product(name: "NIOPortForwarding", package: "swift-nio-portforwarding"),
+				.product(name: "Logging", package: "swift-log"),
 			],
 			path: "darwin/Sources/CakeAgentLib",
 			swiftSettings: [
