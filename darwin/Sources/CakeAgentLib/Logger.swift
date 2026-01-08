78d943f9152bb722670bb9108ad801d20bb57ebd
Fred78290 <frederic.boltz@gmail.com>
2026-01-08 15:46:51 +0100

    Improves Logger TTY detection and metadata
    Refactors the Logger to determine TTY support by checking the TERM environment variable for color support.
    
    Adds metadata to log messages, providing context like file, function, and line number.

--- darwin/Sources/CakeAgentLib/Logger.swift
+++ darwin/Sources/CakeAgentLib/Logger.swift
@@ -107,17 +107,29 @@ public final class Logger {
 
 	public static var prefix: String = ""
 
+	private static func isColouredTTY(file: FileHandle) -> Bool {
+		guard FileHandle.standardOutput.isTTY() else {
+			return false
+		}
+		
+		guard let term = ProcessInfo.processInfo.environment["TERM"] else {
+			return false
+		}
+
+		return term.contains("color")
+	}
+
 	public init(_ target: Any) {
 		let thisType = type(of: target)
 		self.logger = Logging.Logger(label: "\(Self.prefix)\(String(describing: thisType))")
 		self.logger.logLevel = Self.logLevel.level
-		self.isTTY = FileHandle.standardOutput.isTTY()
+		self.isTTY = Self.isColouredTTY(file: FileHandle.standardOutput)
 	}
 
 	public init(_ label: String) {
 		self.logger = Logging.Logger(label: "\(Self.prefix)\(label)")
 		self.logger.logLevel = Self.logLevel.level
-		self.isTTY = FileHandle.standardOutput.isTTY()
+		self.isTTY = Self.isColouredTTY(file: FileHandle.standardOutput)
 	}
 
 	static public func Level() -> LogLevel {
@@ -132,58 +144,58 @@ public final class Logger {
 		Self.logLevel = level
 	}
 
-	public func error(_ err: Error) {
+	public func error(_ err: Error, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.error {
 			if self.isTTY {
-				logger.error("\u{001B}[0;31m\u{001B}[1m\(String(stringLiteral: err.localizedDescription))\u{001B}[0m")
+				logger.error("\u{001B}[0;31m\u{001B}[1m\(String(stringLiteral: err.localizedDescription))\u{001B}[0m", metadata: metadata(), file: file, function: function, line: line)
 			} else {
-				logger.error(.init(stringLiteral: err.localizedDescription))
+				logger.error(.init(stringLiteral: err.localizedDescription), metadata: metadata(), file: file, function: function, line: line)
 			}
 		}
 	}
 
-	public func error(_ err: String) {
+	public func error(_ err: String, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.error {
 			if self.isTTY {
-				logger.error("\u{001B}[0;31m\u{001B}[1m\(String(stringLiteral: err))\u{001B}[0m")
+				logger.error("\u{001B}[0;31m\u{001B}[1m\(String(stringLiteral: err))\u{001B}[0m", metadata: metadata(), file: file, function: function, line: line)
 			} else {
-				logger.error(.init(stringLiteral: err))
+				logger.error(.init(stringLiteral: err), metadata: metadata(), file: file, function: function, line: line)
 			}
 		}
 	}
 
-	public func warn(_ line: String) {
+	public func warn(_ message: String, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.warning {
 			if self.isTTY {
-				logger.warning("\u{001B}[0;33m\u{001B}[1m\(String(stringLiteral: line))\u{001B}[0m")
+				logger.warning("\u{001B}[0;33m\u{001B}[1m\(String(stringLiteral: message))\u{001B}[0m", metadata: metadata(), file: file, function: function, line: line)
 			} else {
-				logger.warning(.init(stringLiteral: line))
+				logger.warning(.init(stringLiteral: message), metadata: metadata(), file: file, function: function, line: line)
 			}
 		}
 	}
 
-	public func info(_ line: String) {
+	public func info(_ message: String, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.info {
-			logger.info(.init(stringLiteral: line))
+			logger.info(.init(stringLiteral: message), metadata: metadata(), file: file, function: function, line: line)
 		}
 	}
 
-	public func debug(_ line: String) {
+	public func debug(_ message: String, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.debug {
 			if self.isTTY {
-				logger.debug("\u{001B}[0;32m\u{001B}[1m\(String(stringLiteral: line))\u{001B}[0m")
+				logger.debug("\u{001B}[0;32m\u{001B}[1m\(String(stringLiteral: message))\u{001B}[0m", metadata: metadata(), file: file, function: function, line: line)
 			} else {
-				logger.debug(.init(stringLiteral: line))
+				logger.debug(.init(stringLiteral: message), metadata: metadata(), file: file, function: function, line: line)
 			}
 		}
 	}
 
-	public func trace(_ line: String) {
+	public func trace(_ message: String, metadata: @autoclosure () -> Logging.Logger.Metadata? = nil, file: String = #fileID, function: String = #function, line: UInt = #line) {
 		if Self.logLevel >= LogLevel.trace {
 			if self.isTTY {
-				logger.trace("\u{001B}[0;34m\u{001B}[1m\(String(stringLiteral: line))\u{001B}[0m")
+				logger.trace("\u{001B}[0;34m\u{001B}[1m\(String(stringLiteral: message))\u{001B}[0m", metadata: metadata(), file: file, function: function, line: line)
 			} else {
-				logger.trace(.init(stringLiteral: line))
+				logger.trace(.init(stringLiteral: message), metadata: metadata(), file: file, function: function, line: line)
 			}
 		}
 	}
